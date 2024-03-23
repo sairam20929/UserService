4aefb338959dcd567dd45403f2c88638011dbe17
sai ram <sairam20929@gmail.com>
2024-03-23 16:33:15 +0530

    small change for null check

--- src/main/java/com/scaler/userservice/controller/AuthController.java
+++ src/main/java/com/scaler/userservice/controller/AuthController.java
@@ -5,10 +5,7 @@ import com.scaler.userservice.model.SessionStatus;
 import com.scaler.userservice.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -50,6 +47,13 @@ public class AuthController {
         }
 
         return new ResponseEntity<>(sessionStatus, HttpStatus.OK);
+    }
+
+    @GetMapping("/goto-google")
+    public ResponseEntity<Void> redirectToGoogle() {
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header("Location", "https://www.google.com") // Replace with desired Google URL
+                .build();
     }
 
 }
--- src/main/java/com/scaler/userservice/security/SecurityConfig.java
+++ src/main/java/com/scaler/userservice/security/SecurityConfig.java
@@ -64,13 +64,11 @@ public class SecurityConfig {
     @Order(2)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
             throws Exception {
-        http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated()
-                )
-                // Form login handles the redirect to the login page from the
-                // authorization server filter chain
-                .formLogin(Customizer.withDefaults());
+
+        http.authorizeHttpRequests((authorize) -> authorize
+                .anyRequest().authenticated()
+        ).formLogin(formLogin -> formLogin
+                .defaultSuccessUrl("/auth/goto-google", true));
 
         return http.build();
     }
